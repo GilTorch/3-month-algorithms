e2a1da63db7ffcf8cb67eb2bbbfabcbbb6e61678
GilTorch <infotorch2014@gmail.com>
2023-11-18 15:18:00 -0400

    Completed first exercise

---
+++ 1/plusMinus.ts
@@ -0,0 +1,76 @@
+// ## Step 1
+
+// ### Understand the problem
+
+// 1. Restate the problem in your own words
+// 2. What are the outputs that should come from the solution of the problem?
+// 3. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you're set about solving the problem. That's okay, it's still worth considering the question at this early stage)
+// 4. How should I label the important pieces of data that are a part of the problem?
+
+// ## Step 2
+
+// ### Explore concrete examples
+
+// 1. Start with simple examples
+// 2. Progress to more complex examples
+// 3. Explore examples with empty inputs
+// 4. Explore examples with invalid inputs 
+
+// ## Step 3
+
+// - Break it down
+// *this forces you to think about the code before you write it (break the code in different steps), and helps you catch any lingering conceptual issues or misunderstanding before you dive in and have to worry about the details as well.*
+
+// ## Step 4
+
+// - Solve/Simplify 
+// *Solve the problem... and if you can't... solve a simpler problem*
+
+//   To Simplify the problem: 
+
+// - Find the core difficulty in what you're trying to do
+// - Temporarily ignore that difficulty
+// - Write a simplified solution
+// - Then incorporate that difficulty back in
+
+// ## Step 5
+
+// - Look Back & Refactor:
+
+// Questions to ask when refactoring: 
+
+// - Can you check the result ?  (Make sure it works)
+// - Can you derive the result differently?
+// - Can you understand it at a glance?
+// - Can you improve the performance of your solution?
+// - Can you think of other ways to refactor ? (guidelines, linting)
+// - How have other people solved this problem ?
+
+
+function plusMinus(arr) {
+  // 
+  let positivesCount = 0; 
+  let zerosCount = 0; 
+  let negativesCount = 0; 
+  const total = arr.length;
+
+  for(let i = 0; i < arr.length; i++){
+    const element = arr[i];
+    if(element > 0){
+        positivesCount++;
+    }else if(element < 0){
+        negativesCount++;
+    }else {
+        zerosCount++;
+    }
+  }
+
+  const positivesRatio = positivesCount / total; 
+  const negativesRatio = negativesCount / total; 
+  const zerosRatio = zerosCount / total;
+
+  console.log((positivesRatio.toFixed(6));
+  console.log(negativesRatio.toFixed(6));
+  console.log(zerosRatio.toFixed);
+
+}
