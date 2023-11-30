a8ae13e88388e319ec7d8e7ea028241a37fc0848
GilTorch <infotorch2014@gmail.com>
2023-11-30 17:46:23 -0400

    Add Hackerrank link

--- week2/2/gradingStudents.ts
+++ week2/2/gradingStudents.ts
@@ -1,5 +1,5 @@
 
-
+// Hackerrank Challenge: https://www.hackerrank.com/challenges/three-month-preparation-kit-grading/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
 const gradingStudents = (grades: number[]): number[] => {
 
     const finalGrades: number[] = [];
