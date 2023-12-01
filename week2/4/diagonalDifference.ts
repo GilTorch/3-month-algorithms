8b11336e3020fa5cfdda58459f62b8c4a9e5861f
GilTorch <infotorch2014@gmail.com>
2023-12-01 03:16:23 -0400

    Update comment

--- week2/4/diagonalDifference.ts
+++ week2/4/diagonalDifference.ts
@@ -1,4 +1,4 @@
-// https://www.hackerrank.com/challenges/three-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+//Hackerrank Challenge: https://www.hackerrank.com/challenges/three-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
 function diagonalDifference(arr: number[][]): number {
     // Write your code here
     let leftDiagonal = 0;
