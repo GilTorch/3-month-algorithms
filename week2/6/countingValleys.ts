8574d8283f6c55f8c75e88baaf9e82f716d4d6ca
GilTorch <infotorch2014@gmail.com>
2023-12-03 08:33:41 -0400

    solve counting valleys

---
+++ week2/6/countingValleys.ts
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+const countingValleys = (steps, path: string):number => {
+    let stepLevelCount = 0; 
+    let valleysCount = 0;
+
+    for(let i = 0; i < steps; i++){
+        let previousStepLevelCount = stepLevelCount;
+        stepLevelCount+= path[i] === "U" ? 1 : -1;
+        if(previousStepLevelCount < 0 && stepLevelCount === 0){
+            valleysCount++;
+        }
+    }
+
+    return valleysCount;
+}
