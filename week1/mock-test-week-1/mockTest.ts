69c077b4a7ec38da59fbdb6424c85684f429baff
GilTorch <infotorch2014@gmail.com>
2023-12-05 03:36:33 -0400

    new solution for mock test 1

--- week1/mock-test-week-1/mockTest.ts
+++ week1/mock-test-week-1/mockTest.ts
@@ -1,4 +1,12 @@
-function findMedian(arr) {
-    // Write your code here
+function findMedian(arr: number[]):number {
+    const n = arr.length;
+    const sortedArray = arr.sort((a,b) => a - b);
+    const mathematicalIndex = (n+1)/2
+    let medianIndex = mathematicalIndex - 1;
+    let median = sortedArray[medianIndex];
 
+    return median;
 }
+
+console.log(findMedian([5,3,2,1,4]))
+console.log(findMedian([0,1,2,4,6,5,3]))
